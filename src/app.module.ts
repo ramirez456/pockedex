83c891eb99560f7af5653913fdcf865f9f6aaf12
Max Houston Ramirez Martel <maxhoustonramirezmartel@Maxs-MacBook-Pro.local>
2022-08-21 10:42:38 -0500

    fix(validateSchema): validate the .env in the configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,12 @@ import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { ConfigModule } from '@nestjs/config';
 import {EnvConfigurations } from './config/app.config';
+import { JoiValidationSchema } from './config/joi.validation';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load:[EnvConfigurations]
+      load:[EnvConfigurations],
+      validationSchema: JoiValidationSchema
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
